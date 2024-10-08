8bee2fd2ad29e51f3254537b8de3ce7212e22abf
Allan Kleber <allankleber@MacBook-Air-de-Allan.local>
2024-10-07 21:43:41 -0300

    Finalizacao do crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 
 @Controller('users')
@@ -7,4 +7,44 @@ export class UserController{
     async create(@Body() body){
         return{body};
     }
+
+    @Get()
+    async read(){
+        return{user:[]}
+    }
+
+    @Get(':id')
+    async readOne(@Param() param){
+        return {user:[],param}
+    }
+
+    @Put(':id')
+    async update(@Body() body,@Param() params){
+
+        return {
+            method: 'put',
+            body,
+            params
+        }
+
+    }
+
+    @Patch(':id')
+    async updateParcial(@Body() body,@Param() params){
+        return {
+            method: 'path',
+            body,
+            params
+        }
+
+    }
+
+    @Delete(':id')
+    async delete(@Param() params){
+        return{
+            params
+        }
+    }
+
+
 }
